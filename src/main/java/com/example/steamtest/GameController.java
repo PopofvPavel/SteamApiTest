b6ae72956d94c165c2b7439ee339402ef0772c12
PopofvPavel <popofvpavel@yandex.ru>
2024-03-06 17:45:07 +0300

    first commit

---
+++ src/main/java/com/example/steamtest/GameController.java
@@ -0,0 +1,22 @@
+package com.example.steamtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GameController {
+
+    private final SteamApiClient steamApiClient;
+
+    public GameController(SteamApiClient steamApiClient) {
+        this.steamApiClient = steamApiClient;
+    }
+
+    @GetMapping("/game/{appId}")
+    public String getGameDescription(@PathVariable long appId) {
+        return steamApiClient.getGameDescription(appId);
+    }
+}
---
+++ src/main/java/com/example/steamtest/SteamApiClient.java
@@ -0,0 +1,27 @@
+package com.example.steamtest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class SteamApiClient {
+
+    @Value("${steam.api.key}") // Подставьте ваш API ключ Steam из application.properties
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+
+
+    public SteamApiClient() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public String getGameDescription(long appId) {
+        System.out.println("In get desctiption method");
+        String url = "https://store.steampowered.com/api/appdetails?appids=" + appId;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        return response.getBody();
+    }
+}
